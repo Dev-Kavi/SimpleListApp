a0f4f8d1fa160e5dbd92c2d28385bab6d23161a6
clv <dev.carllouissevisda@gmail.com>
2025-03-29 18:09:17 +0800

    Code cleanup removed unused classes

--- app/src/main/java/com/dev/carl/simplelistapp/MainActivity.kt
+++ app/src/main/java/com/dev/carl/simplelistapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dev.carl.simplelistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
--- app/src/main/java/com/dev/carl/simplelistapp/data/models/UserListEntry.kt
+++
@@ -1,7 +0,0 @@
-package com.dev.carl.simplelistapp.data.models
-
-data class UserListEntry(
-    val name: String,
-    val userName: String,
-    val email: String
-)
